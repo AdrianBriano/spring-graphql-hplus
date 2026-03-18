35bf5c38757b18048362103ddd3192be88217205
Adrian Briano <adrian.briano.garcia@outlook.com>
2026-03-18 15:54:20 -0600

    Adding a mutation

--- src/main/java/com/adrian/spring/graphql/hplus/controller/CustomerController.java
+++ src/main/java/com/adrian/spring/graphql/hplus/controller/CustomerController.java
@@ -1,8 +1,10 @@
 package com.adrian.spring.graphql.hplus.controller;
 
 import com.adrian.spring.graphql.hplus.data.Customer;
+import com.adrian.spring.graphql.hplus.data.CustomerInput;
 import com.adrian.spring.graphql.hplus.data.CustomerRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -27,5 +29,10 @@ public class CustomerController {
     @QueryMapping
     public Customer customerByEmail(@Argument String email) {
         return  customerRepository.findCustomerByEmail(email);
+    }
+
+    @MutationMapping
+    public Customer addCustomer(@Argument(name="input") CustomerInput customerInput) {
+        return customerRepository.save(customerInput.getCustomerEntity());
     }
 }
